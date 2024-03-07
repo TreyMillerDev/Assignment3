72eb74c5a2b084f6dc17aeba29d0779e0b149eb3
DatN <datjn@uci.edu>
2024-03-07 02:14:52 +0000

    post m2

--- helper_funcs.py
+++ helper_funcs.py
@@ -2,8 +2,43 @@ import pickle
 import json
 import ujson
 import os
+import math
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
- 
+
+def find_the_best_docs(tokens_dict):
+
+    #tokens dict is
+    #{term : { docid : (termfreq , [pos] ) } }
+
+    returnable = []
+    
+    if len(tokens_dict.keys()) == 1: # sort by frequency if there is just one token
+        onlykey = list(tokens_dict.keys())[0] # makes a key of the only token
+        returnable = sorted(tokens_dict[onlykey], key=lambda x: tokens_dict[onlykey][x][0], reverse=True) #sorts in order of most frequent to least frequent
+        
+
+    else:
+
+        new_tokens_dict = dict()
+        for token in tokens_dict.keys(): #for each key
+            docids = sorted(tokens_dict[token], key=lambda x: tokens_dict[token][x][0], reverse=True) #sorts in order of most frequent to least frequent
+            new_tokens_dict[token] = docids #add the docids
+
+
+        concat_urls = []
+
+        for token in new_tokens_dict.keys(): #compiles all the docids into one list
+            concat_urls += new_tokens_dict[token]
+
+        #sorts based on most docid hits. if they are the same, then the frequency sorting from above takes precedent.
+        concat_urls = sorted(concat_urls, key=lambda x: concat_urls.count(x), reverse=True)
+
+        for docid in concat_urls:
+            if docid not in returnable:
+                returnable.append(docid)
+
+    return returnable
+
 def sort_JSONS_into_pickle(): #sort pkl files MUST CREATE A DIRECTORY : sortedJSON
     for letter in alphabet:
         replacement_dict = dict()
@@ -13,6 +48,7 @@ def sort_JSONS_into_pickle(): #sort pkl files MUST CREATE A DIRECTORY : sortedJS
             # print(data)
             for token, list_tups in data.items(): # seperate the key from the value 
                 replacement_dict[token] = dict()
+
             #     # token = word 
             #     # list_tups = [(.00124, 3, 15),(.00124, 3, 15),(.00124, 3, 15) ]
             # for item in list_tups:
@@ -22,7 +58,13 @@ def sort_JSONS_into_pickle(): #sort pkl files MUST CREATE A DIRECTORY : sortedJS
                     else:
                         replacement_dict[token][item[1]][1].append(item[2])
 
-                # data = pickle.load(fp) # the dictionary kinda of nightmare
+            for key in replacement_dict.keys():
+                changed = replacement_dict[key] # gives us a dictionary 
+                for key in changed.keys():
+                    tuple_of_changed = changed[key]
+                    changed[key] = (tuple_of_changed[0] * (math.log(40389/ len(changed))), tuple_of_changed[1])
+
+                # sorts the dictionary inside replacement dictionary 
             with open(f"sortedJSON/{letter}.pkl", 'wb') as fj:
                 pickle.dump(replacement_dict,fj)
 
@@ -58,7 +100,7 @@ def retrieve_word(word):
 
 if __name__ == "__main__":
     # sort_JSONS_into_pickle()
-    # print(retrieve_word("ahmedn"))
+    print(retrieve_word("machi"))
     # {22728: (0.0020408163265306124, [104]), 3674: (0.0040650406504065045, [73]), 3842: (0.0040650406504065045, [73])}
     # print(get_url(22728))
     # print(get_url(3674))
@@ -67,4 +109,4 @@ if __name__ == "__main__":
 # https://www.ics.uci.edu/~kkask/Fall-2014%20CS271/index.html
 # https://iasl.ics.uci.edu/people/ahmedn/#levorato-ahmed-2014-smartgridcomm
 # http://iasl.ics.uci.edu/people/ahmedn/
-    find_file("http://iasl.ics.uci.edu/people/ahmedn/", 'DEV/iasl_ics_uci_edu/')
+    # find_file("https://www.ics.uci.edu/~iftekha/publication/applying-mutation-analysis-on-kernel-test-suites-an-experience-report/", 'DEV/www_ics_uci_edu/')
--- main.py
+++ main.py
@@ -3,16 +3,52 @@ import ujson
 import time
 from datadump import alpha_sort, push_to_disk
 from bs4 import BeautifulSoup
+from helper_funcs import retrieve_word, get_url
 # doc:  https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 
 import nltk
 from nltk.stem import PorterStemmer
-# doc: https://www.nltk.org/, and class 
+# doc: https://www.nltk.org/, and class
+# from spellchecker import SpellChecker
 
 #this is the function to process the html files 
 #as of right now it grabs the important text BUT DOESNT DO ANYTHING WITH it
 #for now its just returning all the text not including the important ones)
 #later we might use the important ones for something
+
+
+def find_the_best_docs(tokens_dict):
+
+    #tokens dict is
+    #{term : { docid : (termfreq , [pos] ) } }
+
+    returnable = []
+    
+    if len(tokens_dict.keys()) == 1: # sort by frequency if there is just one token
+        onlykey = list(tokens_dict.keys())[0] # makes a key of the only token
+        returnable = sorted(tokens_dict[onlykey], key=lambda x: tokens_dict[onlykey][x][0], reverse=True) #sorts in order of most frequent to least frequent
+    else:
+        new_tokens_dict = dict()
+        for token in tokens_dict.keys(): #for each key
+            docids = sorted(tokens_dict[token], key=lambda x: tokens_dict[token][x][0], reverse=True) #sorts in order of most frequent to least frequent
+            new_tokens_dict[token] = docids #add the docids
+
+
+        concat_urls = []
+
+        for token in new_tokens_dict.keys(): #compiles all the docids into one list
+            concat_urls += new_tokens_dict[token]
+
+        #sorts based on most docid hits. if they are the same, then the frequency sorting from above takes precedent.
+        concat_urls = sorted(concat_urls, key=lambda x: concat_urls.count(x), reverse=True)
+        # for loop is for unqiue docIDs
+        for docid in concat_urls:
+            if docid not in returnable:
+                returnable.append(docid)
+
+    return returnable
+
+
 def process_html(content):
     soup = BeautifulSoup(content, 'html.parser')
 
@@ -154,3 +190,68 @@ def file_processor(directory="DEV"):
     print(f"Average document length: {avg_doc_length} tokens")
     print(f"Processing time: {processing_time:.03f}ms")
 
+# here is the simple loop that ask user for query (typing exit to exit)
+# once web GUI is implemented we will replace this with web GUI
+
+def validate_query(query):
+    # spelling check initialization
+    # spell = SpellChecker()
+    # tokenize user input
+    query_tokens = nltk.word_tokenize(query)
+    
+    refined_tokens = []
+
+    for token in query_tokens:
+        # Handle acronyms (fully upper case tokens)
+        if token.isupper():
+            refined_tokens.append(token.lower())
+            continue
+        # check for capitalized tokens
+        # lowercasing capitalized tokens might not be desirable in all cases especially for proper nouns
+        if token[0].isupper():
+            refined_tokens.append(token.lower())
+            continue
+        else:
+            # spelling correction 
+            # corrected = spell.correction(token)
+            refined_tokens.append(token)
+            # now lemmatize term using WordNet
+            # this works for for some inputs that are miss typed but it also affects correct input so this requires more work
+            #synonyms = wordnet.synsets(corrected)
+            #if synonyms:
+            #    # get the most general word
+            #    lemma_names = synonyms[0].lemma_names()
+            #    if lemma_names:
+            #        corrected = lemma_names[0]  # Taking the first synonym as the corrected term
+
+            # refined_tokens.append(refined_tokens)
+            
+    return refined_tokens
+
+if __name__ == "__main__":
+    while True:
+            
+            user_query = input("Enter your search query (or type exit to quit): ").strip()
+            if user_query.lower() == "exit":
+                break
+            # start = time.time()
+            valid_token = validate_query(user_query)
+# iftekhar ahmed
+            print(f"Valid token: {valid_token}")
+            empty_dict = dict()
+            stemmer = PorterStemmer()
+            for token in valid_token:
+                stem_token = stemmer.stem(token)
+                dicts_word = retrieve_word(stem_token)
+                if dicts_word != None:
+                    empty_dict[stem_token] = retrieve_word(stem_token)
+                    # print(len(dicts_word))
+            
+            for docID in find_the_best_docs(empty_dict)[0:5]:
+                print(get_url(docID))
+
+            # end = time.time()
+            # print((end - start))
+                    # print(get_url(docID))
+                    # for id in list(dicts_word.keys())[0:5]:
+                    #     print(get_url(id))
